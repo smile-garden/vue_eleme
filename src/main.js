807f70ee28968221a105d8fb6419679b58e3259f
1539027251@qq.com <1539027251@qq.com>
2018-05-29 20:06:15 +0800

    'first'

---
+++ src/main.js
@@ -0,0 +1,42 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from './components/goods/goods';
+import ratings from './components/ratings/ratings';
+import seller from './components/seller/seller';
+
+import './common/stylus/index.styl';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.config.productionTip = false;
+
+const routes = [{
+  path: '/',
+  component: goods
+}, {
+  path: '/goods',
+  component: goods
+}, {
+  path: '/ratings',
+  component: ratings
+}, {
+  path: '/seller',
+  component: seller
+}];
+
+const router = new VueRouter({
+  linkActiveClass: 'active',
+  routes
+});
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+});
